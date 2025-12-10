e36c1a91412d04fd475e77bdd025d110667436a6
aguaman123 <anderson.guaman@udla.edu.ec>
2025-12-10 17:53:31 -0500

    replica 2

--- app/server.js
+++ app/server.js
@@ -56,7 +56,7 @@ app.get('/', async (req, res) => {
     }
     
     res.json({
-        message: '🚀 Microservicio  Jenkins + ArgoCD + LaunchDarkly Ander  5 replica',
+        message: '🚀 Microservicio  Jenkins + ArgoCD + LaunchDarkly Ander  2 replica',
         version: process.env.APP_VERSION || '1.0.0',
         environment: process.env.NODE_ENV || 'development',
         timestamp: new Date().toISOString(),
